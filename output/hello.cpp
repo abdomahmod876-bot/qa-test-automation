812f502d3f7d401b9ed8b892d87c98481cc21805
Abdo Mahmood <abdomahmod876@gmail.com>
2026-04-15 21:09:04 +0000

    add task

---
+++ output/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name, mobile;
+    int age;
+    cout << "Enter your name: " << endl;
+    cin >> name;
+    cout << "Enter your age: " << endl;
+    cin >> age;
+    cout << "Enter your mobile number: " << endl;
+    cin >> mobile;
+    cout << "Your name is " << name << endl;
+    cout << "Your age is " << age << endl;
+    cout << "Your mobile number is " << mobile << endl;
+    return 0;
+}
