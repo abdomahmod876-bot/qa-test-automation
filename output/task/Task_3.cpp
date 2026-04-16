8a3c864bc7f1460c72e68351d4f9aa3bf028c680
Abdo Mahmood <abdomahmod876@gmail.com>
2026-04-16 08:50:13 +0000

    update Task_3.cpp

--- output/task/Task_3.cpp
+++ output/task/Task_3.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    // هذا برنامج بسيط يطلب من المستخدم إدخال ثلاثة أعداد، ثم يحسب مجموعها ومتوسطها ويعرض النتيجة.
     float a, b, c;
     cout << "Enter three numbers: " << endl;
     cin >> a >> b >> c;
